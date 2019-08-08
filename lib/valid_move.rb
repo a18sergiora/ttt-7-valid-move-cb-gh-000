52438e4767457f4b21a13c35b840e32ef87b0b3a
Sergio Romeu Agraso <a18sergiora@iessanclemente.net>
2019-08-08 23:07:35 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -11,7 +11,7 @@ end
 
 # code your #valid_move? method here
 def valid_move?(board,index)
-  if position_taken?(board,index) && index.between?(0,8)
+  if !position_taken?(board,index) && index.between?(0,8)
     return true
   else
     return false
